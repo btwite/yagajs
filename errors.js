fb7ac0e53057cd3f3180df75d4a48662d1a68f36
Bruce Twite <BTwite@corp.marlo.com.au>
2018-07-11 21:36:45 +1000

    Save

--- errors.js
+++ errors.js
@@ -5,6 +5,47 @@
  * 
  */
 
+function YagaError(element, msg) {
+    _setErrorDetails(this, element, msg);
+    _captureStackTrace(this, module.exports.YagaError);
+    return (this);
+}
+_inheritErrorPrototype(YagaError, Error);
+
+function BinderError(ctxt, msg) {
+    _setErrorDetails(this, ctxt.element(), msg);
+    _captureStackTrace(this, module.exports.BinderError);
+    this.context = () => ctxt;
+    return (this);
+}
+_inheritErrorPrototype(BinderError, YagaError);
+
+function CastError(e, prot) {
+    _setErrorDetails(this, e, `${e.typeName} cannot be cast to ${prot.typeName}`);
+    _captureStackTrace(this, module.exports.CastError);
+    this.prot = () => prot;
+    return (this);
+}
+_inheritErrorPrototype(BinderError, YagaError);
+
+let _evaluateType = {
+    EXPRESSION: 'EXPRESSION',
+    RISTIC: 'RISTIC',
+    PIPELINE: 'PIPELINE',
+    PRIMITIVE: 'PRIMITIVE',
+    CONTEXT: 'CONTEXT',
+    EVALUATOR: 'EVALUATOR',
+    INDEX: 'INDEX',
+    PARSE: 'PARSE'
+};
+
+function _setErrorDetails(that, element, msg) {
+    that.message = msg;
+    that.element = function () {
+        return (element);
+    }
+}
+
 function _captureStackTrace(that, fnError) {
     if (typeof Error.captureStackTrace === 'function') {
         Error.captureStackTrace(that, fnError);
@@ -14,20 +55,26 @@ function _captureStackTrace(that, fnError) {
 function _inheritErrorPrototype(fnError, fnProt) {
     fnError.prototype = Object.create(fnProt.prototype);
     fnError.prototype.constructor = fnError;
-    fnError.prototype.name = fnError.name;
+    fnError.prototype.name = 'yaga.' + fnError.name;
 }
-
-function YagaError(element, msg) {
-    this.message = msg;
-    this.e = element;
-    _captureStackTrace(this, YagaError);
-}
-_inheritErrorPrototype(YagaError, Error);
-
-let err = new YagaError(100, 'Yaga error');
 
 module.exports = {
+    YagaError: _errorMethod(YagaError),
+    BinderError: _errorMethod(BinderError),
+    BindError: _errorMethod(BindError),
+    CastError: _errorMethod(CastError),
+    EvaluateError: _errorMethod(EvaluateError),
+    FrameError: _errorMethod(FrameError),
+    NameError: _errorMethod(NameError),
+    NamespaceError: _errorMethod(NamespaceError),
+    ParserError: _errorMethod(ParserError),
+    PrimitiveError: _errorMethod(PrimitiveError),
+    RisticValidationError: _errorMethod(RisticValidationError),
 
+    evaluateType: _evaluateType,
 };
-
 Object.freeze(module.exports);
+
+function _errorMethod(fnError) {
+    return ((...args) => new fnError(...args));
+}
